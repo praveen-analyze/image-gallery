001ce9eb993e7634d9bafbca1ca3db758a3c2d4b
praveen-analyze <epraveen952@example.com>
2026-03-29 11:25:17 +0530

    final commit

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
-    <div style={{ textAlign: "center", padding: "2px", backgroundColor: "#282c34", color: "white", marginBottom:"10px", position:"sticky",top:"0",zIndex: 1000,
-      boxShadow: "0 2px 10px rgba(0,0,0,0.2)" }}>
+    <div style={{ textAlign: "center", padding: "10px", backgroundColor: "white", marginBottom:"10px", position:"sticky",top:"0",zIndex: 1000,
+      boxShadow: "0 2px 10px rgba(0,0,0,0.2)",display:"flex", justifyContent:"space-between" }}>
       <h1 style={{ margin: 0, fontSize: "2.5rem" }}>Nature Gallery</h1>
       <p>A collection of beautiful moments</p>
     </div>
